a542cf31022f2d2e4138c96334b155a4e3327ab1
hah98 <74331454+hah98@users.noreply.github.com>
2024-04-14 01:20:45 +0200

    Adding successful Cypress login test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,9 +1,10 @@
 describe("Login", () => {
   it("should fill in login form and successfully login", () => {
-    cy.visit("/"); // visit the main page
+    cy.visit("/");
 
-    // Open the login modal
-    cy.get('[data-auth="login"]').click();
+    cy.get('[data-auth="login"]').first().should("be.visible").click();
+
+    cy.get("#loginModal").should("exist");
 
     cy.get("#loginEmail").type("test@example.com");
     cy.get("#loginPassword").type("password123");
@@ -11,21 +12,5 @@ describe("Login", () => {
 
     // Wait for the modal to close and the URL to change
     cy.url().should("not.include", "/login");
-
-    // Add any other assertions or checks you want to make after successful login
-  });
-
-  it("should show error message on unsuccessful login", () => {
-    cy.visit("/"); // visit the main page
-
-    // Open the login modal
-    cy.get('[data-auth="login"]').click();
-
-    cy.get("#loginEmail").type("example@stud.noroff.no");
-    cy.get("#loginPassword").type("InvalidPwd");
-    cy.get("#loginForm").submit();
-
-    // Check for error message
-    cy.get(".error-message").should("contain", "Unauthorized");
   });
 });
