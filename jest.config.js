69f7f63f962b447e803c79f4513560996638a861
hah98 <74331454+hah98@users.noreply.github.com>
2024-04-14 02:04:36 +0200

    Added successful test using Jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   testEnvironment: "jsdom",
   testMatch: ["<rootDir>/tests/**/*.test.js"],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+  },
+  setupFilesAfterEnv: ["./setupTests.js"],
 };
--- tests/login.test.js
+++ tests/login.test.js
@@ -1,10 +1,18 @@
 import { login } from "../src/js/api/auth/login.js";
 
 describe("Login Function", () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
+
   it("should login successfully and save token", async () => {
-    const profile = await login("test@example.com", "password123");
-    expect(profile).toHaveProperty("accessToken");
-    expect(profile).toHaveProperty("profile");
+    fetch.mockResponseOnce(
+      JSON.stringify({ profile: { accessToken: "testToken", profile: {} } }),
+    );
+
+    const response = await login("test@example.com", "password123");
+
+    expect(response.profile).toHaveProperty("accessToken");
   });
 
   it("should throw error on unsuccessful login", async () => {
